dfd377f64743e7b8a59c8a0b9dc254871dde24a5
imadityacoder <iamadityacoder01@gmail.com>
2023-11-12 12:36:00 +0530

    start

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("home")
+
+def index(request):
+    return HttpResponse("home")
+
+def index(request):
+    return HttpResponse("home")
+
+def index(request):
+    return HttpResponse("home")
+
